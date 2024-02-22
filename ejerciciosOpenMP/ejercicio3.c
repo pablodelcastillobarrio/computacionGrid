b809f54cd85bbde10378e58d161d16e403479929
pablodelcastillobarrio <pdelcb00@estudiantes.unileon.es>
2024-02-22 16:10:45 +0100

    ejercicio for nowait

--- ejerciciosOpenMP/ejercicio3.c
+++ ejerciciosOpenMP/ejercicio3.c
@@ -13,23 +13,24 @@ int main (){
 	chunk=CHUNKSIZE;
 	//Las variables a, b, c, chunk son de tipo compartido
 	//Las variables i, tid son privadas a cada thread
+	
     int num_threads = 3;
     omp_set_num_threads(num_threads);
 	
     #pragma omp parallel private(tid, i) shared(a, b, c, chunk)
-	//...............
 	{
 		//Asignación estática de iteraciones en la que el 
 		//tamaño del bloque se fija a chunk
-        #pragma omp for schedule(static, chunk)
-		//............................
+        #pragma omp for schedule(static, chunk) nowait
 		for (i=0;i<N;i++){
-			//.........Get ID of thread
-			//.........Get number of threads
+			//Get ID of thread
+			//Get number of threads
             tid = omp_get_thread_num(); 
             nthreads = omp_get_num_threads();
 			c[i]=a[i]+b[i];
 			printf("El thread %d, de %d threads, calcula la iteración i = %d\n", tid, nthreads, i);
 		}
+		printf("El thread %d acaba", omp_get_thread_num());
 	} //Fin de la región paralela
+	printf("Fin del paralelismo");
 }

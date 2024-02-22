97dcc7c6a486dccddc3d678e5aa168eac07d69b4
pablodelcastillobarrio <pdelcb00@estudiantes.unileon.es>
2024-02-22 15:46:35 +0100

    Ejercicio1 bien

--- ejerciciosOpenMP/ejercicio1.c
+++ ejerciciosOpenMP/ejercicio1.c
@@ -2,25 +2,45 @@
 #include <omp.h>
 
 int main(){
-    int nthreads, tid;
-    printf("Set 4 threads\n");
-    //Set number of threads to 4
+	int nthreads, tid;
+	printf("Trabajamos con 4 threads\n");
+	
+	//.......Set number of threads to 4
+	//.......Obtain the number of threads
     int num_threads = 4;
-    omp_set_num_threads(num_threads); 
-    //Obtain the number of threads
+    omp_set_num_threads(num_threads);
     nthreads = omp_get_num_threads();
-    printf("Number of threads = %d\n", nthreads);
-    #pragma omp parallel private(tid)
-    {
-        //Get the id of the thread
+	
+	printf("Número de threads en ejecución = %d\n", nthreads);
+	#pragma omp parallel private(tid) //Región paralela. Cada 
+	{	//thread contiene sus propias copias de variables
+		//........Get the id of the thread
         tid = omp_get_thread_num(); 
-        printf("Hello from thread = %d\n",tid);
-        if (tid == 0){ 
-            //If I am the master
-            //Obtain the number of threads
+		printf("Hola desde el thread = %d\n",tid);
+		if (tid == 0){ //If I am the master
+			//.........Obtain the number of threads
             nthreads = omp_get_num_threads();
-            printf("Number of threads = %d\n", nthreads);
-        }
-    }
-    return 0;
+			printf("Número de threads = %d\n", nthreads);
+		}
+	}
+	//Todos los threads se unen al maestro y finalizan
+	printf("Trabajamos ahora con 3 threads");
+	//.......Set number of threads to 3
+	//.......Obtain the number of threads
+    num_threads = 3;
+    omp_set_num_threads(num_threads);
+    nthreads = omp_get_num_threads();
+
+	printf("Número de threads en ejecución = %d\n",nthreads);
+	#pragma omp parallel private(tid)
+	{
+		//........Get the id of the thread
+        tid = omp_get_thread_num(); 
+		printf("Hola desde el thread = %d\n",tid);
+		if (tid == 0){ //If I am the master
+			//......Obtain the number of threads
+            nthreads = omp_get_num_threads();
+			printf("Número de threads = %d\n", nthreads);
+		}
+	}
 }
